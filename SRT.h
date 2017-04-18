49b08638752e5d3292276b94d75ca07948b75cf2
Wong Yiu Hang <henrymail2006@ymail.com>
2017-04-19 00:27:48 +0800

    Update: SRT function

--- SRT.h
+++ SRT.h
@@ -8,6 +8,16 @@
  ***************************************************/
 
 struct Schedule* SRT_algorithm(struct Queue* jobQueue) {
+	int day, line;
 	struct Schedule* scheduleTable = malloc(sizeof(schedule_t));
+	struct Queue* cloneJobQueue = jobQueue;
+	//cloneJobQueue = cloneQueue(jobQueue);
+
+	for(day = 0; day < 60; day++) {
+		// Initialise value
+		for(line = 0; line < 3; line++)
+			scheduleTable->day[day].orderID[line] = -1;
+	}
+
 	return scheduleTable;
 }
