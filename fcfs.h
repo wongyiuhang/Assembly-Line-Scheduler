cb0a7bcc66d95d585219c8533e9051e36fc86b38
Wong Yiu Hang <henrymail2006@ymail.com>
2017-04-22 04:28:20 +0800

    Update: FCFS Module

--- fcfs.h
+++ fcfs.h
@@ -10,22 +10,46 @@ int FCFS_daysleft(int num){
 int FCFS_addOrderToSchedule(struct Schedule* scheduleTable, struct Order* order, int day, int line) {
 }
 
-int FCFS_isConflict() {
+int FCFS_isConflict(const struct Order* order, const struct Order* todayOrder[3]) {
 }
 
 //assume order are ordered by start date
 void FCFS_algorithm(struct Queue* jobQueue, char* outputPath, struct Schedule* resultScheduleTable){
 	// Initialization
-	int i, j;
+	int day, line;
 	struct Queue* cloneJobQueue = cloneQueue(jobQueue);
+	struct Node* nextNode;
+	struct Order* firstOrder;
 	
-	for(i = 0; i < NUM_OF_DAY; i++) {
-		for(j = 0; j < 3; j++) {
-			// Know the first order
-			// Find the best start point of the order
-			isConflict();
-			// Add order to the position
-			FCFS_addOrderToSchedule(struct Schedule* scheduleTable, struct Order* order, int day, int line);
+	for(day = 0; day < NUM_OF_DAY; day++) {
+		// Initialise value
+		int skipDay = 0;
+		struct DayJob* today = resultScheduleTable->days + day;
+		struct Order* todayOrder[3], buffer;
+		for(line = 0; line < 3; line++) {
+			todayOrder[line] = NULL;
+			today->orderID[line] = 0;
 		}
+
+		for(line = 0; line < 3; line++) {
+			// Know the first order
+			nextNode = cloneJobQueue->head;
+			firstOrder = (nextNode != NULL) ? nextNode->data : NULL;
+
+			// Find the best start point of the order
+			if(FCFS_isConflict(firstOrder, todayOrder)) {
+				skipDay = 1;
+				break;
+			}
+
+			// Add order to the position
+			FCFS_addOrderToSchedule(resultScheduleTable, firstOrder, day, line);
+			dequeue(cloneJobQueue, buffer);
+		}
+
+		if(skipDay)
+			continue;
+
+		// Pass DayJob to file output process
 	}
 }
